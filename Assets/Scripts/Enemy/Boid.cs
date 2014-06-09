71c12288265c8eee6a4a691f73879368f62acb6d
Bui Phi Quan <rongxanh2052004@yahoo.com>
2014-06-09 23:29:43 +0700

    new code

---
+++ Assets/Scripts/Enemy/Boid.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class Boid : MonoBehaviour {
+	
+	private Vector3 velocity = Vector3.zero;
+	public float mass = 30.0f;
+	public SteeringBehavior steering;
+	
+	private bool start = false;
+	
+	void Awake() {
+		steering.Init(this);
+		start = true;
+	}
+	
+	public virtual void Init(Vector3 pos) {
+	  transform.position = pos;
+		steering.Init(this);
+		start = true;
+		// steering 	= new SteeringManager(this);
+		// x = position.x;
+		// y = position.y;
+	}
+	
+	public float GetAngle(Vector3 vector) {
+		return Mathf.Atan2(vector.y, vector.x);
+	}
+	
+	public void Update() {
+	  if (start) {
+  		steering.Seek(GameObject.Find("Ship").transform.position);
+  		steering.DoUpdate();
+
+  		// transform.position = position;
+
+  		// x = position.x;
+  		// y = position.y;
+
+  		// Adjust boid rodation to match the velocity vector.
+      // transform.rotation = Quaternion.Euler(new Vector3(90 + (180 * GetAngle(velocity)) / Mathf.PI, transform.rotation.y, transform.rotation.z));
+
+  		// if (position.x < 0 || position.x > Screen.width || position.y < 0 || position.y > Screen.height) {
+  		// 	Reset();
+  		// }
+	  }
+	}
+	
+	public void Reset() {
+	}
+	
+	public Vector3 Velocity {
+		get {return velocity;}
+		set {velocity = value;}
+	}
+	
+	public float GetMaxVelocity() {
+		return 0.5f;
+	}
+	
+	public Vector3 GetPosition() {
+		return transform.position;
+	}
+	
+	public float GetMass() {
+		return mass;
+	}
+}
---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyManager : MonoBehaviour {
+
+  public GameObject[] listEnemyPrefabs;
+
+	// Use this for initialization
+	void Start () {
+	  for (int i = 0; i < 5; i++) {
+	    Vector3 screenPos = new Vector3(Random.Range(0, Screen.width), Random.Range(0, Screen.height), 0);
+      Vector3 pos = Camera.main.ScreenToWorldPoint(screenPos);
+  	  GameObject go = GameObject.Instantiate(listEnemyPrefabs[i % 2]) as GameObject;
+  	  go.name = "Enemy_Fast";
+  	  Boid boid = go.GetComponent<Boid>();
+  	  boid.Init(pos);
+	  }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
